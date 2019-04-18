ac27b83d4c794a1cc718a873712a75d5edc7c201
Emilio Coppa <ercoppa@gmail.com>
2019-04-18 17:13:09 +0200

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,40 @@
 var app = new Vue({
+
     el: '#app',
+
     data: {
-        publications: []
+        publications: [],
+        citations: [],
+        showBib: -1
     },
+
     methods: {
-        fetchFromJson: function (jsonPath, vue, v) {
-            $.get(jsonPath, function (json) {
-                vue[v] = json.data;
+
+        fetchPublications: function (path, vue) {
+
+            const Cite = require('citation-js')
+            $.get(path, function (data) {
+                vue.citations = new Cite(data);
+                vue.publications = vue.citations.data;
             })
+        },
+
+        printBib: function (c) {
+            const Cite = require('citation-js');
+            return (new Cite(c)).format('bibtex').replace(/\t/g, " ");
+        },
+
+        showBibEntry: function (k) {
+            if (this.showBib == k)
+                this.showBib = -1;
+            else
+                this.showBib = k;
         }
     },
+
     created() {
-        this.fetchFromJson('assets/json/publications.json', this, 'publications')
+        this.fetchPublications('assets/bib/publications.bib', this);
     }
 
-})
+});
+
